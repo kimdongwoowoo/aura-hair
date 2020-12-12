82fe98e81e1ad80a545728c666673a993c6ab96a
DESKTOP-P36E5DF\KDW <mallocation.naver.com>
2020-12-12 18:17:39 +0900

    일정추가form

--- routes/API/index.js
+++ routes/API/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var customerRouter = require('./customer');
 var productRouter = require('./product');
 var salesRouter = require('./sales');
+var scheduleRouter = require('./schedule');
 /* GET home page. */
 // /api/
 router.get('/', function(req, res, next) {
@@ -11,4 +12,5 @@ router.get('/', function(req, res, next) {
 router.use('/customer',customerRouter);
 router.use('/product',productRouter);
 router.use('/sales',salesRouter);
+router.use('/schedule',scheduleRouter);
 module.exports = router;
---
+++ routes/API/schedule.js
@@ -0,0 +1,93 @@
+var express = require('express');
+var router = express.Router();
+var calendar = [
+    {
+        title: 'All Day Event',
+        start: '2020-09-01'
+    },
+    {
+        title: 'Long Event',
+        start: '2020-09-07',
+        end: '2020-09-10'
+    },
+    {
+        groupId: 999,
+        title: 'Repeating Event',
+        start: '2020-09-09T16:00:00'
+    },
+    {
+        groupId: 999,
+        title: 'Repeating Event',
+        start: '2020-09-16T16:00:00'
+    },
+    {
+        title: 'Conference',
+        start: '2020-09-11',
+        end: '2020-09-13'
+    },
+    {
+        title: 'Meeting',
+        start: '2020-09-12T10:30:00',
+        end: '2020-09-12T12:30:00'
+    },
+    {
+        title: 'Lunch',
+        start: '2020-09-12T12:00:00'
+    },
+    {
+        title: 'Meeting',
+        start: '2020-09-12T14:30:00'
+    },
+    {
+        id: 155,
+        title: 'Happy Hour',
+        allDay: true,
+        start: '2020-09-12T17:30:00'
+    },
+    {
+        title: 'Dinner',
+        start: '2020-09-12T20:00:00'
+    },
+    {
+        title: 'Birthday Party',
+        start: '2020-09-13T07:00:00'
+    },
+    {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2020-09-28'
+    }
+];
+router.use(express.json());
+
+router.get('/', (req, res, next) => {
+    res.send(calendar);
+
+});
+
+router.post('/', (req, res, next) => {
+    var schedule = req.body;
+    schedule.id = calendar.length + 1;
+    calendar.push(schedule);
+    res.send(schedule);
+
+});
+
+
+router.put("/:id", (req, res) => {
+    var schedule;
+    for (var i = 0; i < calendar.length; ++i) {
+
+        if (calendar[i].id == parseInt(req.params.id)) {
+            schedule = calendar[i] = req.body;
+            break;
+        }
+
+    }
+    if (schedule) {
+        res.send(sales);
+    } else {
+        res.status(404).send('ID was not found');
+    }
+});
+module.exports = router;
